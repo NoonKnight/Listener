161cc685d317d39ea80a1681f3609018202e59c6
Jeff Siebold <noonknight@gmail.com>
2018-04-13 10:51:23 -0400

    Added MainForm class

--- Listener/Program.cs
+++ Listener/Program.cs
@@ -13,7 +13,7 @@ namespace Listener {
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault( false );
-			Application.Run( new Form() );
+			Application.Run( new MainForm() );
 		}
 	}
 }
